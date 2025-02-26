3a09913ebc9aa7eed249c15bf786c6b30ff1ad4a
Ayushkumar025 <ayushkumar0117@gmail.com>
2025-02-27 02:45:58 +0530

    logut success

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+const userModel=require('../models/user.model');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+
+
+module.exports.authUser=async(req,res,next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    console.log(token,"this is token");
+    
+    if(!token){
+        return res.status(401).json({errors:[{msg:'unauthorized token'}]})
+    }
+
+    const isBlackListed = await userModel.findOne({ token:token });
+
+    if(isBlackListed){
+        return res.status(401).json({message: 'unauthorized'})
+    }
+
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        const user= await userModel.findById(decoded._id);
+        
+        
+
+
+        req.user=user;
+        console.log(user,"ayush");
+        
+        next();
+    }
+    catch(err){
+        return res.status(401).json({errors:[{msg:'Unauthorized'}]})
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const {body}=require('express-validator')
 const userController=require('../controllers/user.controller');
+const authmiddleware=require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -18,5 +19,7 @@ router.post('/login',[
     userController.loginUser
 )
 
+router.get('/profile',authmiddleware.authUser,userController.getUserProfile)
+router.get('/logout',authmiddleware.authUser,userController.logoutUser)
 
 module.exports=router;
